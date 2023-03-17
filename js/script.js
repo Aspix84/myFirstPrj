f4c844f41ab9669985d5e9f910598de360147713
Драгоморецкий А.С <dos@mk.fse.gov.ua>
2023-03-17 14:56:50 +0200

    Init: initial marckup.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// let sum = 0;
+// while (true) {
+//   let value = +prompt("Input a number", "");
+//   if (!value) break;
+//   if (value % 2 == 0) continue;
+//   sum += value;
+// }
+
+// console.log("Summ = " + sum);
+
+// let i = 3;
+
+// while (i) {
+//   alert(i--);
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2) continue;
+//   else alert(i);
+// }
